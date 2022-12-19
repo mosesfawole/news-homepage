c1b98d56c77db28d5b0ed92dbc9fa759f10e8721
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-19 17:16:48 +0100

    start hero section

--- news-homepage/src/components/Hero.jsx
+++ news-homepage/src/components/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="hero">
+        <div className="d-image"></div>
+        <div className="mobile-image"></div>
+        <div className="contents">
+          <div className="bigTexts">
+            <h1></h1>
+          </div>
+          <div className="smallTexts">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
