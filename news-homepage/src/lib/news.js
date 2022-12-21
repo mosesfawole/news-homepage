0b390a3a00cf95286e154205a157dfb8686649a9
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-21 10:17:02 +0100

    create news card js file

---
+++ news-homepage/src/lib/news.js
@@ -0,0 +1,24 @@
+import Image1 from "../images/image-retro-pcs.jpg";
+import Image2 from "../images/image-top-laptops.jpg";
+import Image3 from "../images/image-gaming-growth.jpg";
+
+const news = [
+  {
+    image: Image1,
+    id: 01,
+    title: "Reviving Retro PCs",
+    text: "What happens when old PCs are given modern upgrades?",
+  },
+  {
+    image: Image2,
+    id: 02,
+    title: "Top 10 Laptops of 2022",
+    text: "Our best picks for various needs and budgets.",
+  },
+  {
+    image: Image3,
+    id: 03,
+    title: "The Growth of Gaming",
+    text: "How the pandemic has sparked fresh opportunities.",
+  },
+];
