82b7d75e42de040826bde2b68e2959056c238cdd
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-21 10:21:06 +0100

    start newscard.jsx

--- news-homepage/src/components/NewsCard.jsx
+++ news-homepage/src/components/NewsCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import news from "../lib/news";
 const NewsCard = () => {
-  return <div>NewsCard</div>;
+  return (
+    <div>
+      <div className="">
+        <ul>
+          {news.map((item, index) => (
+            <div key={index}>
+              <li>
+                <div className="left">
+                  <img src={item.image} alt={item.id} />
+                </div>
+                <div className="right">
+                  <p>{item.id}</p>
+                  <h1>{item.title}</h1>
+                  <p>{item.text}</p>
+                </div>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NewsCard;
