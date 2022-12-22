8f1e6b0144303a7536d8d1a18dc89c37a3a3e515
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-22 07:58:27 +0100

    mobile dropdown complete

--- news-homepage/src/components/Header.jsx
+++ news-homepage/src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../images/logo.svg";
 import Hamburger from "../images/icon-menu.svg";
+import CloseIcon from "../images/icon-menu-close.svg";
 import navs from "../lib/navbar";
+
 const Header = () => {
+  const [isMenu, setIsMenu] = useState(false);
+
+  const openMenu = () => {
+    setIsMenu(true);
+  };
+
+  const closeMenu = () => {
+    setIsMenu(false);
+  };
+
   return (
     <div>
       {/* mobile menu */}
@@ -10,17 +22,25 @@ const Header = () => {
         <div className="logo">
           <img src={Logo} alt="logo" />
         </div>
-        <div className="menu">
+        <div className="menu-icon" onClick={openMenu}>
           <img src={Hamburger} alt="hamburger" />
         </div>
-        <div className="menu-content hidden">
-          <ul>
-            {navs.map((item) => (
-              <li className="cursor-pointer" key={item}>
-                {item}
-              </li>
-            ))}
-          </ul>
+        <div className={isMenu ? "flex" : "hidden"}>
+          <div className="overlay fixed right-0 top-0 w-full z-30 h-full bg-[#00000036]"></div>
+          <div className="bg-white flex flex-col gap-20 fixed right-0 top-0 p-6 z-30 w-[70%] h-full">
+            <span className="flex justify-end" onClick={closeMenu}>
+              <img src={CloseIcon} alt="clos menu icon" />
+            </span>
+            <ul className="flex flex-col gap-4">
+              {navs.map((item) => (
+                <>
+                  <li className="cursor-pointer   text-2xl" key={item}>
+                    {item}
+                  </li>
+                </>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
 
